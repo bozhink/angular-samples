ec3a50d472b754162ce1bce00993e4fe72798d6c
Bozhin Karaivanov <bozhink@users.noreply.github.com>
2017-08-20 18:17:30 +0300

    Update

--- angular1/Todo/todo.js
+++ angular1/Todo/todo.js
@@ -1,19 +1,28 @@
 (function (angular) {
     'use strict';
 
+    var id = 0;
+
+    function getId() {
+        return id += 1;
+    }
+
     angular.module('todoApp', [])
         .controller('TodoListController', function() {
             var todoList = this;
             todoList.todos = [{
+                id: getId(),
                 text: 'learn angular',
                 done: true
             }, {
+                id: getId(),
                 text: 'build an angular app',
                 done: false
             }];
 
             todoList.addTodo = function() {
                 todoList.todos.push({
+                    id: getId(),
                     text: todoList.todoText,
                     done: false
                 });
@@ -22,18 +31,15 @@
 
             todoList.remaining = function() {
                 var count = 0;
-                angular.forEach(todoList.todos, function(todo) {
-                    count += todo.done ? 0 : 1;
-                });
+                angular.forEach(todoList.todos, (todo) => count += todo.done ? 0 : 1);
                 return count;
             };
 
             todoList.archive = function() {
                 var oldTodos = todoList.todos;
                 todoList.todos = [];
-                angular.forEach(oldTodos, function(todo) {
-                    if (!todo.done) todoList.todos.push(todo);
-                });
+                angular.forEach(oldTodos, (todo) => todo.done ? null : todoList.todos.push(todo));
             };
         });
-}(window.angular))
+
+}(window.angular));
